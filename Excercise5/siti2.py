941eba8e7b91a6e5f521f9a49434bc1e219a0a9a
Dkrii <dikri.ali_ti23@nusaputra.ac.id>
2024-03-28 13:49:26 +0700

    Exercise5

---
+++ Excercise5/siti2.py
@@ -0,0 +1,32 @@
+goods = f"""
+1. lipstic 20pcs
+2. bedak 20pcs
+3. parpum 20pcs"""
+print(goods)
+user = int(input("masukan barang yang ingin kamu beli :"))
+if user == 1 :
+    print("Harga dari Lipstik Rp. 15,000/pcs")
+    pengguna = int(input("Berapa banyak lipstic yang ingin kamu beli :"))
+    if pengguna < 20 :
+        price = pengguna * 15000
+        print(f"Harga yang harus kamu bayar : Rp.{price:,}")
+    else :
+        print("Stock tidak memenuhi")
+elif user == 2 :
+    print("Harga dari bedak Rp. 10,000/pcs")
+    pengguna = int(input("Berapa banyak bedak yang ingin kamu beli :"))
+    if pengguna < 20 :
+        price = pengguna * 10000
+        print(f"Harga yang harus kamu bayar : Rp.{price:,}")
+    else :
+        print("Stock tidak memenuhi")
+elif user == 3 :
+    print("harga dari parfum Rp. 20,000/pcs")
+    pengguna = int(input("Berapa banyak parfum yang ingin kamu beli :"))
+    if pengguna < 20 :
+        price = pengguna * 20000
+        print(f"Harga yang harus kamu bayar : Rp.{price:,}")
+    else :
+        print("Stock tidak memenuhi")
+else :
+    print("Not pound")
