9dfe0c003d945dde5da47a68de2573022423a4f6
Dkrii <dikri.ali_ti23@nusaputra.ac.id>
2024-03-28 14:20:01 +0700

    Excercise4

---
+++ Exercise6/A.py
@@ -0,0 +1,14 @@
+score = int(input("nilai:"))
+
+if(score > 90):
+    print("nilai anda A")
+elif(score > 80):
+    print("nilai anda B")
+elif(score > 70):
+    print("nilai anda C")
+elif(score > 60):
+    print("nilai anda D")
+elif(Score > 0):
+    print("nilai anda F")
+else:
+    print("maaf anda salah input")
